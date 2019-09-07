3a16e183877f513ffde26a8a1d1913d757c2a59c
Eric R. Kock <eric.r.kock@gmail.com>
2019-09-07 18:36:59 +0100

    Updates

---
+++ precache-manifest.ac7433e7d0280f3d0bb43d88aba57520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7739390856adb201d86331da9b6eb19a",
+    "url": "/Character-Count/index.html"
+  },
+  {
+    "revision": "e35ebc970046296075e3",
+    "url": "/Character-Count/static/css/main.a4dc67f9.chunk.css"
+  },
+  {
+    "revision": "ad7956f7cfe30d8d952e",
+    "url": "/Character-Count/static/js/2.9b2488c7.chunk.js"
+  },
+  {
+    "revision": "e35ebc970046296075e3",
+    "url": "/Character-Count/static/js/main.9c80292f.chunk.js"
+  },
+  {
+    "revision": "6982ff964f44a6457271",
+    "url": "/Character-Count/static/js/runtime~main.ed3f8e42.js"
+  }
+]);
